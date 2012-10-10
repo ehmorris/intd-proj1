4695bd0701bb3901a3417e7e90d99f0d5a0bdd8d
Edwin <edwin.h.morris@gmail.com>
2012-10-10 01:42:56 -0400

    add form fields, javascript section switching

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 try{Typekit.load();}catch(e){}
+
+$(function() {
+
+  // hide all sections
+  $('section').hide();
+
+  // show first section by default
+  $('section').first().show();
+
+  // show next pane
+  $('.action a.next').click(function() {
+    if ($(this).parents('section').next('section').length) {
+      $(this).parents('section').hide();
+      $(this).parents('section').next('section').show();
+    }
+    return false;
+  });
+
+});
